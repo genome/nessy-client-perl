537a95c051fb0df60ba74e0c7963743a794935c2
Anthony Brummett <abrummet@genome.wustl.edu>
2014-02-04 13:06:16 -0600

    add _is_released property to protect against double releasing

--- lib/GSCLockClient/Claim.pm
+++ lib/GSCLockClient/Claim.pm
@@ -3,7 +3,7 @@ package GSCLockClient::Claim;
 use strict;
 use warnings;
 
-use GSCLockClient::Properties qw(resource_name keychain);
+use GSCLockClient::Properties qw(resource_name keychain _is_released);
 
 sub new {
     my ($class, %params) = @_;
@@ -17,7 +17,12 @@ sub new {
 
 sub release {
     my $self = shift;
-    return $self->keychain->release( $self->resource_name );
+    return if $self->_is_released();
+    return unless $self->keychain;
+
+    my $rv =  $self->keychain->release( $self->resource_name );
+    $self->_is_released(1);
+    return $rv;
 }
 
 sub DESTROY {
