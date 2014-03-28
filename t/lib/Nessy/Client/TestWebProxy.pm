97e60824d1a726b0e91a0605c4d3ac53d2c2e93d
Michael J. Kiwala <michael.kiwala@gmail.com>
2014-03-28 13:19:26 -0500

    Fix bug in recording content length

--- t/lib/Nessy/Client/TestWebProxy.pm
+++ t/lib/Nessy/Client/TestWebProxy.pm
@@ -120,10 +120,10 @@ sub _read_request_from_socket {
     my($content_length, $body_length) = (0,0);
     while(1) {
         $do_read->();
-        if ($buf =~ m/\r\n\r\n(.*)/) {
+        if ($buf =~ m/\r\n\r\n(.*)/s) {
             $body_length = length($1);
             # read in all the headers
-            my($content_length) = $buf =~ m/^Content-Length: (\d+)/im;
+            ($content_length) = $buf =~ m/^Content-Length: (\d+)/im;
             last;
         }
     }
