eba2f90b42e236bc878812694119a1d80ada8f96
Anthony Brummett <abrummet@genome.wustl.edu>
2014-04-03 16:34:56 -0500

    add POD

--- lib/Nessy/Claim.pm
+++ lib/Nessy/Claim.pm
@@ -46,3 +46,79 @@ sub DESTROY {
 }
 
 1;
+
+=pod
+
+=head1 NAME
+
+Nessy::Claim - Represent a claim on a resource
+
+=head1 SYNOPSIS
+
+  use Nessy::Client;
+
+  my $client = Nessy::Client->new( url => $server_url );
+
+  my $claim = $client->claim( $resource_name );
+
+  do_something_while_resource_is_locked();
+
+  $claim->release();
+
+=head1 Constructor
+
+Nessy::Claim instances should not be created by calling new() directly on this
+class.  They should instead be instantiated by calling claim() on a
+L<Nessy::Client> instance.
+
+=head2 Methods
+
+=over 4
+
+=item resource_name()
+
+Returns the resource name of this claim.  Set at the time the Nessy::Claim is
+instantiated.
+
+=item release()
+
+Release the claim.  Returns true if the claim was successfully released,
+meaning that the claim was valid at the time of release and the server
+responded successfully when the release was requested.
+
+A release might fail if the claim's ttl has expired (due to the inability to
+contant the server to update the tll), or if the claim was removed on the
+server by an administrator, for example.
+
+C<release()> accepts an optional function ref as an argument used as a callback
+to deliver the result to.  The callback an only be run if the main program
+enters the AnyEvent event loop.
+
+=item validate()
+
+Validate the claim.  Returns true of the claim is still valid, false
+otherwise.
+
+C<validate()> accepts an optional function ref as an argument used as a callback
+to deliver the result to.  The callback an only be run if the main program
+enters the AnyEvent event loop.
+
+=item DESTROY
+
+Releases the claim when the instance goes out of scope; not called directly.
+
+=back
+
+=head1 SEE ALSO
+
+L<Nessy::Client>, L<Nessy::Daemon>
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (C) 2014 Washington University in St. Louis, MO.
+
+This sofware is licensed under the same terms as Perl itself.
+See the LICENSE file in this distribution.
+
+=cut
+
--- lib/Nessy/Client/Message.pm
+++ lib/Nessy/Client/Message.pm
@@ -57,3 +57,90 @@ sub TO_JSON {
 }
 
 1;
+
+=pod
+
+=head1 NAME
+
+Nessy::Client::Message - Transport messages between Nessy::Client and Nessy::Daemon
+
+=head1 DESCRIPTION
+
+Nessy::Client::Message instances are not used directly in a client program, but
+are created by Nessy::Client and Nessy::Daemon to talk to each other.
+
+The Client will create Message objects to request commands be performed by
+the Daemon.  The Message is serialized with JSON and sent to the Daemon over
+their communication socket.  The Daemon performs the action, sets the Message
+as successful or failed, and sends back a message with the same serial,
+filling in the error_message property if there was an exception.
+
+=head1 CONSTRUCTOR
+
+  my $message = Nessy::Client::Message->new(
+                    resource_name => 'scarce_resource',
+                    command => 'claim',
+                    args => { user_data => 'secret', ttl => 321, timeout => 99 },
+                    serial => 123 );
+
+A Message instance essentially represents an RPC request.  resource_name,
+command and serial are required arguments to the constructor.
+
+  my $message = Nessy::Client::Message->from_json( $json_string )
+
+Creates a Message instance from a JSON string.  This is used to deserialize
+the message.
+
+=head2 Methods
+
+=over 2
+
+=item resource_name()
+
+Returns the resource_name set when the object was created
+
+=item command()
+
+Returns the command set when the object was created
+
+=item serial()
+
+Returns the serial set when the object was created
+
+=item error_message()
+
+=item error_message( $exception_message )
+
+Get or set the error_message attribute
+
+=item is_succeeded()
+
+Returns true if this Message has gotten a response and it was successful.
+
+=item success()
+
+Mark this message as successful.  C<is_succeeded> will now return true.
+
+=item is_failed()
+
+Returns true if this Message has gotten a response and it failed.
+
+=item fail()
+
+Mark this message as failed.  C<is_failed> will now return true.
+
+=back
+
+=head1 SEE ALSO
+
+L<Nessy::Claim>, L<Nessy::Daemon>
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (C) 2014 Washington University in St. Louis, MO.
+
+This sofware is licensed under the same terms as Perl itself.
+See the LICENSE file in this distribution.
+
+=cut
+
